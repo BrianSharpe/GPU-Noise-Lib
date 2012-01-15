a1e6685639370cd1b0cd806e106a15a2d8eeaae3
Brian Sharpe <brisharpe@yahoo.com>
2012-01-15 19:14:02 +1300

    Small change to falloff/interpolation functions defined in x-squared

--- gpu_noise_lib.glsl
+++ gpu_noise_lib.glsl
@@ -396,19 +396,12 @@ vec4 Interpolation_C3( vec4 x ) { vec4 xsq = x*x; vec4 xsqsq = xsq*xsq; return x
 
 
 //
-//	Interpolation defined in XSquared
-//	( smoothly increase from 0.0 to 1.0 as xsq increases from 0.0 to 1.0 )
+//	Falloff defined in XSquared
+//	( smoothly decrease from 1.0 to 0.0 as xsq increases from 0.0 to 1.0 )
 //	http://briansharpe.wordpress.com/2011/11/14/two-useful-interpolation-functions-for-noise-development/
 //
-float Interpolation_Xsq_C1( float xsq ) { xsq = 1.0 - xsq; return 1.0 - xsq*xsq; }   //  1.0 - ( 1.0 - x*x )^2
-float Interpolation_Xsq_C2( float xsq ) { float xsqsq = xsq*xsq; return xsqsq * ( 9.0 - 16.0 * xsq + xsqsq * ( 9.0 - xsqsq ) ); }   //  9x^4-16x^6+9x^8-x^12
-float Interpolation_Xsq_C2_Fast( float xsq ) { float xsqsq = xsq*xsq; return xsqsq * ( 5.0 + xsq * ( xsqsq - 5.0 ) ); }   //  5x^4-5x^6+x^10   ( or for an alternative which requires no extra registers 3x^4-3x^8+x^12 )
-float Interpolation_Xsq_C3( float xsq ) { return xsq * xsq * ( 10.0 + xsq * ( xsq * ( 15.0 - 4.0 * xsq ) - 20.0 ) ); }   //  10x^4-20x^6+15x^8-4x^10
-
-//	( smoothly falloff from 1.0 to 0.0 as xsq increases from 0.0 to 1.0 )
-float Falloff_Xsq_C1( float xsq ) { xsq = 1.0 - xsq; return xsq*xsq; }   // ( 1.0 - x*x )^2   ( Used by Humus for lighting falloff in Just Cause 2.  GPUPro 1 )
-float Falloff_Xsq_C2( float xsq ) { xsq = 1.0 - xsq; return xsq*xsq*xsq; }   // ( 1.0 - x*x )^3.   NOTE: 2nd derivative is 0.0 at x=1.0, but non-zero at x=0.0
-
+float Falloff_Xsq_C1( float xsq ) { xsq = 1.0 - xsq; return xsq*xsq; }	// ( 1.0 - x*x )^2   ( Used by Humus for lighting falloff in Just Cause 2.  GPUPro 1 )
+float Falloff_Xsq_C2( float xsq ) { xsq = 1.0 - xsq; return xsq*xsq*xsq; }	// ( 1.0 - x*x )^3.   NOTE: 2nd derivative is 0.0 at x=1.0, but non-zero at x=0.0
 
 
 //
